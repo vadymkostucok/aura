9af7a0a72ffe43f44b0067c04d2b438202c23800
vadymkostucok <vadym.kostucok@gmail.com>
2025-07-30 14:29:57 +0200

    mobile coming soon placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,33 @@ import { Programs } from '@/components/Programs/Programs'
 import { Form } from '@/components/Form/Form'
 import { VideoScrollWrapper } from '@/components/Video'
 import { Header } from '@/components/Header/Header'
+import { Box } from '@/components/Box'
+import { H5 } from '@/components/Typography'
+import { FlexContainer } from '@/components/FlexContainer'
 
 export default function Home() {
     return (
         <>
-            <Header />
-            <VideoScrollWrapper>
-                <Intro />
-                <About />
-                <Podcast />
-                <Report />
-                <Libraries />
-                <Programs />
-            </VideoScrollWrapper>
-            <Form />
+            <Box className="hidden xl:block">
+                <Header />
+                <VideoScrollWrapper>
+                    <Intro />
+                    <About />
+                    <Podcast />
+                    <Report />
+                    <Libraries />
+                    <Programs />
+                </VideoScrollWrapper>
+                <Form />
+            </Box>
+            <FlexContainer className="xl:hidden h-screen" center>
+                <H5 className="capitalize">
+                    {'Don’t Worry. '}
+                    <span className="text-[#21D7A6]">
+                        Mobile Version Coming Soon.
+                    </span>
+                </H5>
+            </FlexContainer>
         </>
     )
 }
