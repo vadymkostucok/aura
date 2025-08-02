9003f3d77e782026b52336d606e2498ca3d6f674
vadymkostucok <vadym.kostucok@gmail.com>
2025-08-02 14:33:34 +0200

    responsive library section

---
+++ src/components/Libraries/LibraryCard.tsx
@@ -0,0 +1,54 @@
+'use client'
+import React from 'react'
+import { Box } from '../Box'
+import { FlexContainer } from '../FlexContainer'
+import Image, { StaticImageData } from 'next/image'
+import { H5, TypographySmall } from '../Typography'
+import Lottie from 'lottie-react'
+
+import crossIcon from '@/assets/cross.svg'
+import singleArrow from '@/assets/singleArrow.json'
+
+type LibraryCardProps = {
+    description: string
+    type: string
+    icon: StaticImageData
+}
+
+export const LibraryCard = ({ type, description, icon }: LibraryCardProps) => {
+    return (
+        <Box>
+            <FlexContainer
+                width="max-w-60"
+                gap="gap-2.5"
+                className="clip-panel-label bg-[#ffffff07] backdrop-blur-md py-1.5 px-4"
+            >
+                <Image src={crossIcon} alt="cross icon" />
+                <TypographySmall className="uppercase opacity-60">
+                    {type}
+                </TypographySmall>
+            </FlexContainer>
+            <FlexContainer
+                className="group p-4 bg-[#ffffff15] hover:bg-[#ffffff05] border border-[#ffffff10] rounded-[2px] hover:text-[#FFF973] backdrop-blur-lg cursor-pointer duration-500"
+                gap="gap-5"
+                alignItems="items-center"
+            >
+                <FlexContainer
+                    width="w-auto"
+                    className="shrink-0 bg-[#00000080] py-3 md:py-4 px-5 md:px-6"
+                >
+                    <Image src={icon} alt={type} className="w-10 md:w-14" />
+                </FlexContainer>
+                <H5 className="tracking-tight leading-none">{description}</H5>
+                <div className="opacity-0 group-hover:opacity-100 absolute right-0 bottom-0 bg-[#00000080] p-2.5 duration-500">
+                    <Lottie
+                        animationData={singleArrow}
+                        loop
+                        autoplay
+                        className="w-5"
+                    />
+                </div>
+            </FlexContainer>
+        </Box>
+    )
+}
