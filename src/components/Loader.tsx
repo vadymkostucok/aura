06997914b5883583a9710f538da4522ecc654d70
vadymkostucok <vadym.kostucok@gmail.com>
2025-08-03 21:11:13 +0200

    minor styling fix

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -31,7 +31,7 @@ export const Loader = () => {
                 <motion.div
                     className="fixed inset-0 z-50 flex items-center justify-center bg-black"
                     initial={{ opacity: 1 }}
-                    exit={{ opacity: 0, transition: { duration: 0.25 } }}
+                    exit={{ opacity: 0, transition: { duration: 0.4 } }}
                 >
                     <motion.div
                         initial={{ scale: 1, opacity: 0 }}
