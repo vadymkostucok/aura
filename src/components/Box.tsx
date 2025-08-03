783b1a4d2e73f9a7b91af6fa7966d1f68cc2bced
vadymkostucok <vadym.kostucok@gmail.com>
2025-08-03 21:11:59 +0200

    box support motion

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode } from 'react'
+import React, { forwardRef, ReactNode } from 'react'
+import clsx from 'clsx'
 
 type BoxProps = {
     id?: string
@@ -7,10 +8,19 @@ type BoxProps = {
     onClick?: () => void
 }
 
-export const Box = ({ id, children, className = '', onClick }: BoxProps) => {
-    return (
-        <div id={id} onClick={onClick} className={`relative ${className}`}>
-            {children}
-        </div>
-    )
-}
+export const Box = forwardRef<HTMLDivElement, BoxProps>(
+    ({ id, children, className = '', onClick }, ref) => {
+        return (
+            <div
+                ref={ref}
+                id={id}
+                onClick={onClick}
+                className={clsx('relative', className)}
+            >
+                {children}
+            </div>
+        )
+    }
+)
+
+Box.displayName = 'Box'
